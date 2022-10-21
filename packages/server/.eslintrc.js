6275682d12dac9a22317716e5c5300052d4c7981
Choover <choover11@gmail.com>
2022-10-21 05:17:26 -0700

    Added eslint

---
+++ packages/server/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'standard-with-typescript',
+    'prettier',
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json'],
+  },
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'error',
+  },
+};
