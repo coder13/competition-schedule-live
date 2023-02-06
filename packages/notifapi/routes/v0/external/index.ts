98197485da5333cb0f139de760d3ba8c8be41f53
Choover <choover11@gmail.com>
2023-02-06 15:26:41 -0800

    Added troubleshooting information

--- packages/notifapi/routes/v0/external/index.ts
+++ packages/notifapi/routes/v0/external/index.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import cors from 'cors';
 import notifyRouter from './notify';
 import admin from './admin';
+import prisma from '../../../db';
 
 const router = Router();
 
@@ -18,6 +19,55 @@ router.use(
 // });
 
 router.use('/admin', admin);
+
+router.use(
+  '/competitions/:competitionId/subscribedUsersCount',
+  async (req, res) => {
+    try {
+      const count = await prisma.competitionSubscription.findMany({
+        select: {
+          userId: true,
+        },
+        distinct: ['userId'],
+        where: {
+          competitionId: {
+            equals: req.params.competitionId,
+            mode: 'insensitive',
+          },
+        },
+      });
+
+      res.json({ success: true, count });
+    } catch (e) {
+      res.status(500).json({ success: false, error: (e as Error)?.message });
+    }
+  }
+);
+
+router.use('/competitions/:competitionId/subscribedUsers', async (req, res) => {
+  try {
+    const users = await prisma.user.findMany({
+      select: {
+        id: true,
+      },
+      where: {
+        CompetitionSubscription: {
+          some: {
+            competitionId: {
+              equals: req.params.competitionId,
+              mode: 'insensitive',
+            },
+          },
+        },
+      },
+    });
+
+    res.json({ success: true, users });
+  } catch (e) {
+    res.status(500).json({ success: false, error: (e as Error)?.message });
+  }
+});
+
 router.use(notifyRouter);
 
 export default router;
