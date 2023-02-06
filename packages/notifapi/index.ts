53f5f083148ca720843f02b92a5d86e1cb8e56c0
Choover <choover11@gmail.com>
2023-02-06 13:34:25 -0800

    fixed cors issue?

--- packages/notifapi/index.ts
+++ packages/notifapi/index.ts
@@ -1,12 +1,18 @@
 import { init } from './server';
 
 const port = process.env.PORT ? +process.env.PORT : 8090;
-const host = process.env.HOST ?? '0.0.0.0';
+const host = process.env.HOST;
 
 init()
-  .then((app) =>
-    app.listen(port, host, () => {
-      console.log('Server is running on port', port);
-    })
-  )
+  .then((app) => {
+    if (host) {
+      app.listen(port, host, () => {
+        console.log('Server is running on port', port, 'host', host);
+      });
+    } else {
+      app.listen(port, () => {
+        console.log('Server is running on port', port);
+      });
+    }
+  })
   .catch((e) => console.error(e));
